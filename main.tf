9ccbf22d525c543cf1fbf595a680f33c03072fc0
martin-pio <63728713+martin-pio@users.noreply.github.com>
2023-01-30 16:38:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,28 @@
+# ####### Only Credentials Starts ####
 provider "aws" {
-  access_key = "AKIAQ4VZD46FPDEY4MEV"
-  secret_key = "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"
+  access_key = "AKIAQ4VZD46FFSTSKCGJ"
+  secret_key = "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"
   region     = "us-east-1"
 }
 
+# ####### Only Credentials Ends  ####
+
+# ####### EC2 Creatio Starts ####
 resource "aws_instance" "ec2_instance" {
   ami = "ami-0b0ea68c435eb488d"
   # count = "${var.number_of_instances}"
-  # subnet_id = "${var.subnet_id}"
+  subnet_id = "subnet-050ecd3a5e4a17fad"
   instance_type = "t2.micro"
-  subnet_id     = "subnet-050ecd3a5e4a17fad"
   # key_name = "${var.ami_key_pair_name}"
-} 
+}
+####### EC2 Creatio Ends ####
+
 
 terraform {
   backend "s3" {
     bucket         = "martin-jenkins-bucket-01"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraformstatefile"
+    dynamodb_table = "terraformstatefile_01"
   }
 }
-
